c3c032a8928cfbc33ab0ddb908a3e2c8aa2713c8
Alegherix <psao69@gmail.com>
2021-02-25 17:05:46 +0100

    added test parallax for HeroText

--- src/components/TestHero.tsx
+++ src/components/TestHero.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useRef, useState, useLayoutEffect, useEffect } from 'react';
 import HeroSVG from './svg/HeroSVG';
 import Image from 'next/image';
 import Navbar from './Navbar';
-import { motion } from 'framer-motion';
+import { motion, useViewportScroll, useTransform } from 'framer-motion';
 
 interface TestHeroProps {}
 
 const TestHero: React.FC<TestHeroProps> = ({}) => {
+  const [elementTop, setElementTop] = useState(0);
+  const ref = useRef(null);
+  const { scrollY } = useViewportScroll();
+
+  const y = useTransform(scrollY, [elementTop, elementTop + 6], [0, -1], {
+    clamp: false,
+  });
+
+  useEffect(() => {
+    const element = ref.current;
+    setElementTop(element.offsetTop);
+  }, []);
+
   return (
-    <main className="relative " style={{ height: '80vh' }}>
+    <main ref={ref} className="relative " style={{ height: '80vh' }}>
       <HeroSVG />
       <Navbar />
-      <div className="mt-10 text-center text-white">
+      <motion.div style={{ y }} className="mt-10 text-center text-white">
         <motion.h1
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.7, ease: 'easeOut' }}
-          style={{ textShadow: '1px 4px 7px rgba(0,0,0,0.2)' }}
+          style={{
+            textShadow: '1px 4px 7px rgba(0,0,0,0.2)',
+          }}
           className="text-5xl mb-2 font-bold"
         >
           Kul med Djur
@@ -30,7 +45,7 @@ const TestHero: React.FC<TestHeroProps> = ({}) => {
         >
           Delsjön runt
         </motion.p>
-      </div>
+      </motion.div>
     </main>
   );
 };
