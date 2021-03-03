e826acd3b2d4d3b51563b688e8ae415a3a626208
Alegherix <psao69@gmail.com>
2021-03-03 08:53:30 +0100

    started implementing Immer for managing events

--- src/hooks/useEvent.tsx
+++ src/hooks/useEvent.tsx
@@ -1,4 +1,40 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
+import produce from 'immer';
+
+interface IEventState {
+  firstEvent: string;
+  secondEvent: string;
+  thirdEvent: string;
+  fourthEvent: string;
+  payload: string;
+}
+
+interface IEventAction {
+  type: string;
+}
+
+const initialState: IEventState = {
+  firstEvent: '',
+  secondEvent: '',
+  thirdEvent: '',
+  fourthEvent: '',
+  payload: '',
+};
+
+const eventReducer = (draft: IEventState, action: IEventAction) => {
+  switch (action.type) {
+    case 'firstEvent': {
+      draft.firstEvent = draft.payload;
+      return;
+    }
+  }
+};
+
+const curriedEventReducer = produce(eventReducer);
+
+const EventUseReducer = () => {
+  const [state, dispatch] = useReducer(curriedEventReducer, initialState);
+};
 
 const EventDispatchContext = createContext({});
 const EventStateContext = createContext({});
