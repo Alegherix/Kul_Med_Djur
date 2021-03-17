5c35988af9bebcd4782499b0d9a84d8e771e50b9
Alegherix <psao69@gmail.com>
2021-03-17 16:17:12 +0100

    added new intro animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,8 @@ import { motion, useTransform, useViewportScroll } from 'framer-motion';
 import React, { useEffect, useRef, useState } from 'react';
 import Navbar from './Navbar';
 import HeroSVG from './svg/HeroSVG';
+import CloudOne from './svg/testClouds/CloudOne';
+import CloudTwo from './svg/testClouds/CloudTwo';
 
 const Hero = () => {
   const [elementTop, setElementTop] = useState(0);
@@ -18,10 +20,19 @@ const Hero = () => {
   }, []);
 
   return (
-    <main ref={ref} className="relative " style={{ height: '80vh' }}>
+    <main
+      ref={ref}
+      className="relative overflow-x-hidden"
+      style={{ height: '80vh' }}
+    >
       <HeroSVG />
       <Navbar />
-      <motion.div style={{ y }} className="mt-10 text-center text-white">
+      <motion.div
+        style={{ y }}
+        className="mt-10 text-center text-white relative"
+      >
+        <CloudOne />
+        <CloudTwo />
         <h1
           style={{
             textShadow: '1px 4px 7px rgba(0,0,0,0.2)',
@@ -30,15 +41,15 @@ const Hero = () => {
         >
           Kul med Djur
         </h1>
-        <motion.p
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.6, duration: 0.6, ease: 'easeOut' }}
+        <p
+          // initial={{ opacity: 0 }}
+          // animate={{ opacity: 1 }}
+          // transition={{ delay: 0.6, duration: 0.6, ease: 'easeOut' }}
           style={{ textShadow: '1px 4px 7px rgba(0,0,0,0.2)' }}
           className="text-xl md:text-3xl"
         >
-          Delsjön runt
-        </motion.p>
+          Delsjön runt - 15/5 2021
+        </p>
       </motion.div>
     </main>
   );
